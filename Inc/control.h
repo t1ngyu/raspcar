f19d50e428dcfd4bd2fb86d2b7ec8c68a2570b03
t1ngyu <229856331@qq.com>
2018-07-05 22:19:44 +0800

    完善PWM和LED控制；

--- Inc/control.h
+++ Inc/control.h
@@ -15,15 +15,6 @@ extern TIM_HandleTypeDef htim1;
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 
-#define htim_drv0          htim2
-#define htim_drv1          htim3
-#define htim_drv2          htim1
-#define htim_drv3          htim2
-#define CHANNEL_DRV0       TIM_CHANNEL_1
-#define CHANNEL_DRV1       TIM_CHANNEL_2
-#define CHANNEL_DRV2       TIM_CHANNEL_1
-#define CHANNEL_DRV3       TIM_CHANNEL_3
-
 void USB_Connect(void);
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
 void Control_Init(void);
--- Src/control.c
+++ Src/control.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include "control.h"
 #include "usbd_custom_hid_if.h"
 
@@ -5,6 +6,16 @@
 #define WHEEL_COUNT     4
 #define LOOP_INTERVAL   10
 #define LED_FLASH_FREQ  10
+
+#define htim_drv0          htim2
+#define htim_drv1          htim3
+#define htim_drv2          htim1
+#define htim_drv3          htim2
+#define CHANNEL_DRV0       TIM_CHANNEL_1
+#define CHANNEL_DRV1       TIM_CHANNEL_2
+#define CHANNEL_DRV2       TIM_CHANNEL_1
+#define CHANNEL_DRV3       TIM_CHANNEL_3
+
 
 
 typedef __packed struct
@@ -17,12 +28,15 @@ typedef __packed struct
 typedef __packed struct
 {
   uint8_t throttles[WHEEL_COUNT];
-} Operation_t;
+  uint8_t led;
+} ControlData_t;
 
-
+static TIM_HandleTypeDef *phtims[] = {&htim2, &htim3, &htim1, &htim2};
+static uint32_t drvChannels[] = {TIM_CHANNEL_1, TIM_CHANNEL_2, TIM_CHANNEL_1, TIM_CHANNEL_3};
 static State_t state;
 static uint32_t tickStart;
-static uint8_t report_buffer[64];
+static uint8_t inReport[64];
+static bool hasOutReportReceived;
 
 
 void USB_Connect(void)
@@ -34,12 +48,13 @@ void USB_Connect(void)
 
 static int8_t CUSTOM_HID_OutEvent_FS(uint8_t event_idx, uint8_t state)
 {
+  hasOutReportReceived = true;
   return (USBD_OK);
 }
 
 void Control_Init(void)
 {
-  int i = 0;
+  int i;
   
   for(i = 0; i < WHEEL_COUNT; i++)
   {
@@ -47,13 +62,10 @@ void Control_Init(void)
     state.throttles[i] = 0;
   }
   state.loopCount = 0;
+  hasOutReportReceived = false;
   
   USBD_CustomHID_fops_FS.OutEvent = CUSTOM_HID_OutEvent_FS;
   USB_Connect();
-  HAL_TIM_PWM_Start(&htim_drv0, CHANNEL_DRV0);
-  HAL_TIM_PWM_Start(&htim_drv1, CHANNEL_DRV1);
-  HAL_TIM_PWM_Start(&htim_drv2, CHANNEL_DRV2);
-  HAL_TIM_PWM_Start(&htim_drv3, CHANNEL_DRV3);
 }
 
 void Sonar(void)
@@ -63,17 +75,41 @@ void Sonar(void)
   //HAL_GPIO_WritePin(SONAR_TRIG_GPIO_Port, SONAR_TRIG_Pin, GPIO_PIN_RESET);
 }
 
-
-void Control_Loop(void)
+void ThrottleControl(uint8_t throttles[])
 {
-  /*
+  int i;
   TIM_OC_InitTypeDef sConfigOC;
   
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 50;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
-  */
+  
+  for (i = 0; i < WHEEL_COUNT; i++)
+  {
+    HAL_TIM_PWM_Stop(phtims[i], drvChannels[i]);
+    sConfigOC.Pulse = throttles[i];
+    HAL_TIM_PWM_ConfigChannel(phtims[i], &sConfigOC, drvChannels[i]);
+    HAL_TIM_PWM_Start(phtims[i], drvChannels[i]);    
+  }
+}
+
+void LedControl(uint8_t on)
+{
+  /* output low for led on */
+  if (on)
+  {
+    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+  }
+  else
+  {
+    HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_SET);
+  }
+}
+
+
+void Control_Loop(void)
+{
+  ControlData_t *cData;
   
   tickStart = HAL_GetTick();
   while (1)
@@ -96,20 +132,17 @@ void Control_Loop(void)
     }
     
     // report status
-    memcpy(report_buffer, &state, sizeof(state));
-    USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, report_buffer, 64);
+    memcpy(inReport, &state, sizeof(state));
+    USBD_CUSTOM_HID_SendReport(&hUsbDeviceFS, inReport, sizeof(inReport));
     
     // control
-    /*
-    HAL_TIM_PWM_Stop(&htim_drv2, CHANNEL_DRV2);
-    sConfigOC.Pulse += 10;
-    if (sConfigOC.Pulse > htim_drv2.Init.Period)
-    {  
-      sConfigOC.Pulse = 0;
+    if (hasOutReportReceived)
+    {
+      hasOutReportReceived = false;
+      cData = (ControlData_t *)((USBD_CUSTOM_HID_HandleTypeDef*)hUsbDeviceFS.pClassData)->Report_buf;
+      ThrottleControl(cData->throttles);
+      //LedControl(cData->led);
     }
-    HAL_TIM_PWM_ConfigChannel(&htim_drv2, &sConfigOC, CHANNEL_DRV2);
-    HAL_TIM_PWM_Start(&htim_drv2, CHANNEL_DRV2);
-    */
   }
 }
 
